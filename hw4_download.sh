fd9804e6af176f164973beb55ed91e799004c427
weiyuan-c <87663784+weiyuan-c@users.noreply.github.com>
2022-12-19 20:30:38 +0800

    Update hw4_download.sh

--- hw4_download.sh
+++ hw4_download.sh
@@ -1,4 +1,4 @@
 # p1
-
+wget https://www.dropbox.com/s/65bdltr6bxbgbc0/fine_last.tar?dl=1 -O fine_last.tar
 # p2
 wget https://www.dropbox.com/s/voeogmapa0bff4l/best_model.pth?dl=1 -O best_model.pth
